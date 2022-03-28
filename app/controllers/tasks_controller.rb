d91875d0b21585e43b84d65c524d2bf2b5d97067
mockysunshinework <mockysunshinework@gmail.com>
2022-03-28 02:06:34 +0000

    C6_タスク新規作成ページを作成。これから詳細ページと編集更新ページを作成する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,18 @@ class TasksController < ApplicationController
   end
   
   def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = @user.tasks.new(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規登録しました。"
+      redirect_to user_tasks_path(@user)
+      
+    else
+      render :new
+    end
   end
   
 
@@ -13,5 +25,9 @@ class TasksController < ApplicationController
   
     def set_user
       @user = User.find(params[:user_id])
+    end
+    
+    def task_params
+      params.require(:task).permit(:name, :description)
     end
 end
