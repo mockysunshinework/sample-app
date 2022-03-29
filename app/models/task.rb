419d10c03ea1cd28691903e30f647863229399d8
mockysunshinework <mockysunshinework@gmail.com>
2022-03-29 15:04:57 +0000

    c6-c8の実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
   
-  validates :name, presence: true, length: { minimum: 5 }
+  validates :name, presence: true
   validates :description, presence: true, length: { minimum: 5 }
 end
