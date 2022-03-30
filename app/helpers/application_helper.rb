12f7c7261d7fa6e25f222cbd59c6211bf60412ef
mockysunshinework <mockysunshinework@gmail.com>
2022-03-30 05:59:23 +0000

    C13_ハイパーリンク実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def text_url_to_link(text)
+    require 'uri'
+      URI.extract(text, ['http', 'https']).uniq.each do |url|
+      sub_text = "<a href=#{url} target=\'_blank\'>#{url}</a>"
+      text.gsub!(url, sub_text)
+      end
+    return text
+  end
+
   def full_title(page_name = "")
     base_title = "Task App"
     if page_name.empty?
