56a6fbaa055d9333e8ebf071c2c794501d57ec28
mockysunshinework <mockysunshinework@gmail.com>
2022-03-30 14:29:11 +0000

    D1_ユーザー編集ページ作成。update action はまだ未着手

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,14 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+  end
+
+
   private
   
     def user_params
